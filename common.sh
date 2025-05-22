6876a5bc0a701ad005daeede21abeb31cd1af2a3
HrushikeshVallapu <hrushikeshvallapu>
2025-05-22 21:57:52 +0530

    using common.sh for functioncalls

--- common.sh
+++ common.sh
@@ -33,6 +33,54 @@ validate(){
     fi
 }
 
+nodejs_setup(){
+    dnf module disable nodejs -y &>>$log_file
+    validate $? "disabling nodejs"
+
+    dnf module enable nodejs:20 -y &>>$log_file
+    validate $? "enabling nodejs:20"
+
+    dnf install nodejs -y &>>$log_file
+    validate $? "installing nodejs:20"
+
+    npm install &>>$log_file
+    validate $? "installing dependencies"
+}
+
+systemd_setup(){
+
+    cp $script_dir/$app_name.service /etc/systemd/system/$app_name.service
+    validate $? "copying $app_name service"
+
+    systemctl daemon-reload &>>$log_file
+    systemctl enable $app_name &>>$log_file
+    systemctl start $app_name
+    validate $? "starting $app_name"
+}
+
+app_setup(){
+    id roboshop &>>$log_file
+    if [ $? != 0 ]
+    then 
+        useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+        validate $? "creating systemuser"
+    else
+        echo -e "$g user already exist $n" 
+    fi
+
+    mkdir -p /app &>>$log_file
+    validate $? "making home dirctry for user"
+
+    curl -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip &>>$log_file
+    validate $? "downloading $app_name zip file"
+
+
+    rm -rf /app/*
+    cd /app
+    unzip /tmp/$app_name.zip &>>$log_file
+    validate $? "unzipping $app_name"
+
+}
 print_time(){
     end_time=$(date +%s)
     total_time=$(($start_time - $end_time))
