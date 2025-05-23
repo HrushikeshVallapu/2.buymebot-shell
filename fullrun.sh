45732461c8f6cf689798d633d1d261c50a14df2b
HrushikeshVallapu <hrushikeshvallapu>
2025-05-23 13:57:42 +0530

    single script for whole process

---
+++ fullrun.sh
@@ -0,0 +1,58 @@
+ami_id="ami-09c813fb71547fc4f"
+sg_id="sg-0142341bd063dfed3"
+instances=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
+zone_id="Z06633071XX7HF3WWN7FZ"
+domain_name="buymebot.shop"
+
+for instance in ${instances[@]}
+#for instance in $@
+do
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0142341bd063dfed3 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    if [ $instance != "frontend" ]
+    then
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+        record_name="$instance.$domain_name"
+    else
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+        record_name="$domain_name"
+    fi
+    echo "$instance IP address: $IP"
+     aws route53 change-resource-record-sets \
+    --hosted-zone-id $zone_id \
+    --change-batch '
+    {
+        "Comment": "Creating or Updating a record set for cognito endpoint"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$record_name'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP'"
+            }]
+        }
+        }]
+    }'
+done
+
+PASSWORD="DevOps321"
+USER="ec2-user"
+
+for service in "${instances[@]}";
+do
+  echo "Connecting to $service"
+
+  sshpass -p "$PASSWORD" ssh -o StrictHostKeyChecking=no "$USER@$service.$domain_name" 'bash -s' <<EOF
+cd /home/ec2-user
+if [ ! -d "devops-shell-roboshop-instances" ]; then
+  git clone https://github.com/HrushikeshVallapu/2.buymebot-shell.git
+fi
+cd devops-shell-roboshop-instances
+git reset --hard HEAD      # Discards local changes
+git pull                   # Pulls latest from remote
+chmod +x $service.sh
+sudo bash $service.sh
+EOF
+
+done
